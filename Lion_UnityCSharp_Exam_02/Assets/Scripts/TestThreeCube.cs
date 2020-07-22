44b1ed18ea15a0159f0ffb75cca4437f6bd4c8ff
paul61302 <paul61302@gmail.com>
2020-07-22 08:45:10 +0800

    123

--- Lion_UnityCSharp_Exam_02/Assets/Scripts/TestThreeCube.cs
+++ Lion_UnityCSharp_Exam_02/Assets/Scripts/TestThreeCube.cs
@@ -21,5 +21,29 @@ public class TestThreeCube : MonoBehaviour
                 Instantiate(cube, new Vector3(i,j,0),Quaternion.identity);
             }
         }
+
+        for (int i = 9; i < 10; i--)
+        {
+            for (int j = 8; j < i; j--)
+            {
+                Instantiate(cube, new Vector3(i+10, j+10, 0), Quaternion.identity);
+            }
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                Instantiate(cube, new Vector3(i, j, 0), Quaternion.identity);
+            }
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                Instantiate(cube, new Vector3(i, j, 0), Quaternion.identity);
+            }
+        }
     }
 }
